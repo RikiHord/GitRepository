610505762e08d48027c7a4b4babbe02adf8db9dd
Riki <MrWoOolf@ukr.net>
2025-12-30 16:19:23 +0200

    Sequrity

---
+++ CodeSimulator/Sequrity/Sequrity/CheckFloor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Sequrity
+{
+    public class CheckFloor
+    {
+        public static bool IsFloorSafe(string input)
+        {
+            string pattern = @"^(?!.*\$[^G]*T)(?!.*T[^G]*\$).*";
+
+            return Regex.IsMatch(input, pattern);
+        }
+    }
+}
---
+++ CodeSimulator/Sequrity/TestSequrity/Test1.cs
@@ -0,0 +1,42 @@
+﻿using Sequrity;
+
+namespace TestSequrity
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test_GuardBetweenMoneyAndThief()
+        {
+            string input = "xTxxxGxx$";
+            bool result = CheckFloor.IsFloorSafe(input);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_NoGuardianAlarm()
+        {
+            string input = "xx$xxxxT";
+            bool result = CheckFloor.IsFloorSafe(input);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_GuardMoneyAndThief()
+        {
+            string input = "xGxx$xxT";
+            bool result = CheckFloor.IsFloorSafe(input);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_MultipleGuardsSafe()
+        {
+            string input = "xGxx$xxGxxT";
+            bool result = CheckFloor.IsFloorSafe(input);
+            Assert.IsTrue(result);
+        }
+
+    }
+
+}
