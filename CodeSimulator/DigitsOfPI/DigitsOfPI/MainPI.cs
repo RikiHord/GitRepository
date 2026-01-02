a8f66e7c3079ea631efdf1a1bb0d15131bc8c550
Riki <MrWoOolf@ukr.net>
2026-01-02 12:13:47 +0200

    DigitsOfPI

---
+++ CodeSimulator/DigitsOfPI/DigitsOfPI/MainPI.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitsOfPI
+{
+    public class MainPI
+    {
+        public static int IndexOfPI(int n)
+        {
+            int maxDigits = 1000;
+            int len = (10 * maxDigits) / 3;
+            List<int> newPi = new List<int>();
+
+            int[] a = new int[len + 1];
+            int nines = 0;
+            int predigit = 0;
+
+            for (int j = 1; j <= len; j++)
+                a[j] = 2;
+
+            for (int j = 1; j <= maxDigits; j++)
+            {
+                int q = 0;
+
+                for (int i = len; i >= 1; i--)
+                {
+                    int x = 10 * a[i] + q * i;
+                    a[i] = x % (2 * i - 1);
+                    q = x / (2 * i - 1);
+                }
+
+                a[1] = q % 10;
+                q /= 10;
+
+                if (q == 9)
+                {
+                    nines++;
+                }
+                else if (q == 10)
+                {
+                    newPi.Add(predigit + 1);
+
+                    for (int k = 1; k <= nines; k++)
+                        newPi.Add(0);
+
+                    predigit = 0;
+                    nines = 0;
+                }
+                else
+                {
+                    if (j > 1)
+                        newPi.Add(predigit);
+
+                    predigit = q;
+
+                    if (nines != 0)
+                    {
+                        for (int k = 1; k <= nines; k++)
+                            newPi.Add(9);
+
+                        nines = 0;
+                    }
+                }
+            }
+
+            return newPi[n];
+        }
+    }
+}
---
+++ CodeSimulator/DigitsOfPI/DigitsOfPI/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitsOfPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Console.ReadLine() is string input && int.TryParse(input, out int value) ? value : 0;
+
+            if (n < 0 || n > 1000)
+            {
+                Console.WriteLine("Please enter a number between 0 and 1000.");
+                return;
+            }
+
+            Console.WriteLine(MainPI.IndexOfPI(n));
+        }
+    }
+}
