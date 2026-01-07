d3582c68a9b9a5d685afe02bc7dedaa284e51332
Riki <MrWoOolf@ukr.net>
2026-01-07 14:46:16 +0200

    Microsoft Learn C# Part 2
    Complete part 2 Foundational C# on freecodecamp

---
+++ MicrosoftLearn/ML_Part2/CsharpProjects/TestProject3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Permissions;
+
+string[] codes = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach (string code in codes)
+{
+    if (code.StartsWith("B"))
+    {
+        Console.WriteLine(code);
+    }
+}
---
+++ MicrosoftLearn/ML_Part2/CsharpProjects/TestProject4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Reverse string message and count char 'o'
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int countCharO = 0;
+
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        countCharO++; 
+    } 
+}
+
+string newMessage = new String(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {countCharO} times.");
