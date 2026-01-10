baee178c2db2fd40ba7fda1cc818733db373aa97
Riki <MrWoOolf@ukr.net>
2026-01-10 13:13:02 +0200

    Microsoft Learn C# Part 5
    Complete part 5 Foundational C# on freecodecamp

---
+++ MicrosoftLearn/ML_Part5/CsharpProjects/TestProject2/Program.cs
@@ -0,0 +1,31 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    DisplayEmail(corporate[i, 0], corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayEmail(external[i, 0], external[i, 1], externalDomain);
+}
+
+void DisplayEmail(string firstname, string lastname, string domain = "contoso.com")
+{
+    string emailName = firstname.Substring(0, 2).ToLower() + lastname.ToLower();
+    string email = $"{emailName}@{domain}";
+    Console.WriteLine(email);
+}
---
+++ MicrosoftLearn/ML_Part5/CsharpProjects/TestProject3/Program.cs
@@ -0,0 +1,46 @@
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+bool ShouldPlay()
+{
+    bool correctInput = false;
+    string? input;
+    do
+    {
+        input = Console.ReadLine();
+        if (input != null && (input.ToLower() == "y" || input.ToLower() == "n"))
+            correctInput = true;
+        else
+            Console.WriteLine("Invalid input. Plese print Y or N");
+    } while (!correctInput);
+
+    return input == "y";
+}
+
+string WinOrLose(int target, int roll)
+{
+    return $"{(target < roll ? "You Win!" : "You lose!")}";
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 7);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(target, roll));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
