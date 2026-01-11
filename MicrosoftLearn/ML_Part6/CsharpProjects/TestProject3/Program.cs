217daa866032ea69bb789f25bcb058642f585245
Riki <MrWoOolf@ukr.net>
2026-01-11 17:27:42 +0200

    Microsoft Learn C# Part 6 Final
    Complete part 6 Foundational C# on freecodecamp

---
+++ MicrosoftLearn/ML_Part6/CsharpProjects/TestProject3/Program.cs
@@ -0,0 +1,63 @@
+﻿string[][] userEnteredValues = new string[][]
+{
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
+
+try{
+    Workflow1(userEnteredValues);
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine();
+}
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try{
+            Process1(userEntries);
+            Console.WriteLine("'Process1' completed successfully.\n");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            try
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values must be valid integers.");
+        }
+    }
+}
